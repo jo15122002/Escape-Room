11bc3df5073b20f52d94ae60c2ac4aac51b47471
JoMobile <lapinette2002@ymail.com>
2023-04-03 21:45:03 +0200

    Limit camera movements on y axis

--- Assets/Scripts/Camera/PlayerCamera.cs
+++ Assets/Scripts/Camera/PlayerCamera.cs
@@ -17,6 +17,17 @@ public class PlayerCamera : MonoBehaviour
     {
         //put the camera at the player's position
         transform.position = player.position;
-        transform.rotation = player.rotation;
+
+        //Rotate the camera on x with the player rotation
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, player.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+
+        float mouseY = Input.GetAxis("Mouse Y");
+        float nextPos = transform.rotation.eulerAngles.x - mouseY;
+        //rotate the camera with the mouse
+        Debug.Log(transform.rotation.eulerAngles.x);
+        if((nextPos <= 89) || (nextPos >= 270))
+        {
+            transform.Rotate(-mouseY, 0, 0);
+        }
     }
 }
