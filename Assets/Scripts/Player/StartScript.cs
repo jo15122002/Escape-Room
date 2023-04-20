33a6feac35666e97baf10eb20107cebe5bbd63fc
JoMobile <lapinette2002@ymail.com>
2023-04-20 17:49:51 +0200

    Mise en place d'un garde fou pour remettre le joueur au spawn au début de partie

--- Assets/Scripts/Player/StartScript.cs
+++ Assets/Scripts/Player/StartScript.cs
@@ -7,6 +7,10 @@ public class StartScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //set the player at the start of the game
+        transform.position = new Vector3(-0.8f, 1.49f, -4.93f);
+        transform.rotation = new Quaternion(0,180,0,0);
+
         HelpText helpText = HelpText.GetHelpText();
         helpText.writeUIText("Bienvenue dans mon jeu!");
         helpText.writeUIText("Demarrons sans plus tarder !");
