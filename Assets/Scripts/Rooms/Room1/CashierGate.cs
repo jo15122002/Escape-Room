2dccbc074be7f92f8f41f7ae9d604d7705e5d7a9
JoMobile <lapinette2002@ymail.com>
2023-04-20 18:16:46 +0200

    ajout de l'ouverture des cashier gates avec une triggerbox

---
+++ Assets/Scripts/Rooms/Room1/CashierGate.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CashierGate : MonoBehaviour
+{
+    [SerializeField] private GameObject cashierGate;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine(OpenGate());
+        }
+    }
+
+    IEnumerator OpenGate()
+    {
+        Quaternion fromRotation = cashierGate.transform.rotation;
+        Quaternion toRotation = Quaternion.AngleAxis(-90f, Vector3.up) * fromRotation;
+        float elapsedTime = 0;
+        float duration = 1f;
+
+        while (elapsedTime < duration)
+        {
+            cashierGate.transform.rotation = Quaternion.Slerp(fromRotation, toRotation, (elapsedTime / duration));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        cashierGate.transform.rotation = toRotation;
+        gameObject.SetActive(false);
+    }
+}
